178e73eba8a1a93e95e6140db5e1d273dfa0a5cb
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-04-14 02:33:53 +0200

    ohfuckohfuckohfuckohfuckohfuckohfuckohfuck

--- cogs/images.py
+++ cogs/images.py
@@ -87,7 +87,7 @@ class images(commands.Cog):
             return await ctx.send("You forgot the image.")
 
         image = Image.open(io.BytesIO(data))
-        image = await resize(image, 1024)
+        image = await resize(image, resolution)
         
         in_scale = out_scale = 1
         dither = True
