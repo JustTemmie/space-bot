6030abe8cb47993c47f25992d9b3be33299151cb
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-04-23 22:22:23 +0200

    automatic commit from server to backup database

--- cogs/github.py
+++ cogs/github.py
@@ -36,7 +36,7 @@ class github(commands.Cog):
             repo.git.add(update=True)
             repo.index.commit("automatic commit from server to backup database")
             origin = repo.remote(name='origin')
-            await ctx.send(origin.push())
+            await ctx.send(subprocess.check_output(origin.push()))
             await ctx.send("Pushed to github")
         except Exception as e:
             await ctx.send(e)
