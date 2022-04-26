5529d06b4fa979772423ae01f078474d6f9574d9
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-04-26 22:28:41 +0200

    marru

--- cogs/marriage.py
+++ cogs/marriage.py
@@ -7,6 +7,7 @@ from discord.ext import commands
 from discord.ext.commands import Cog, Greedy, CheckFailure, command, has_permissions, bot_has_permissions
 import json
 
+import time
 
        
         
@@ -14,8 +15,12 @@ class marry(Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    
+    @commands.command(name = "marry")
+    async def react_henwee(self, ctx, member:discord.Member):
+        await ctx.send("{} is now married to {}".format(ctx.author.mention, member.mention))
+        time.sleep(5)
 
+        await ctx.send("lmao yea as if i'm not storing that too much effort")
 
 def setup(bot):
     bot.add_cog(marry(bot))
