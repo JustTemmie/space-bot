6072cad10c52edd797fcbae5e607a5bb0b882a08
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-05-02 19:20:26 +0200

    we

--- cogs/weather.py
+++ cogs/weather.py
@@ -16,13 +16,14 @@ class weather(Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command(name = "weather", brief = "hey what's the weather?")
+    @commands.command(name = "weather", brief = "hey what's the weather?\n don't specify country or state, you can try using country code or state abbreviation\nbut that may or may not work\ncan only display the current hour's forecast\nwind direction is 0° when going north, 90° when going east, and so on")
     @cooldown(5, 60, BucketType.user)
     async def check_weather(self, ctx, *, input = None):
         if input == None:
             if ctx.guild.id != 918787074801401868:
                 return await ctx.send("Please specify a location.")
             input = "tromsø"
+            await ctx.send("https://www.yr.no/nb/v%C3%A6rvarsel/daglig-tabell/1-305409/Norge/Troms%20og%20Finnmark/Troms%C3%B8/Troms%C3%B8")
 
         r = requests.get(f"https://api.openweathermap.org/geo/1.0/direct?q={input}&limit=1&appid={weather_key}")
         request = json.loads(r.content)
@@ -36,7 +37,7 @@ class weather(Cog):
         my_forecast.update()
         
         forecast = str(my_forecast.data.intervals[0]).split("\n")
-    
+
         embed = Embed(title = f"{forecast[0][:-1]}\nUTC, in {realoutput['name']}, {realoutput['country']}", description = "", color = 0x00ff00)
         
         for a in forecast:
