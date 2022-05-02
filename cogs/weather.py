4e74362a2cb3f138c8fc159a4913c50768fc3537
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-05-02 17:13:55 +0200

    weather lmao

--- cogs/weather.py
+++ cogs/weather.py
@@ -12,8 +12,8 @@ class weather(Cog):
         self.bot = bot
     
     @commands.command(name = "weather", brief = "hey what's the weather?")
-    async def check_weather(self, ctx, *, place = "tromsø"):
-        if place != "tromsø":
+    async def check_weather(self, ctx, *, input = "tromsø"):
+        if input != "tromsø":
             return await ctx.send("error")
 
         place = Place("tromsø", 69.69, 19, 30)
@@ -21,7 +21,18 @@ class weather(Cog):
         my_forecast = Forecast(place, "metno-locationforecast/1.0 https://github.com/Rory-Sullivan/metno-locationforecast")
 
         my_forecast.update()
-        await ctx.send(my_forecast)
+        
+        forecast = str(my_forecast.data.intervals[0]).split("\n")
+    
+        embed = Embed(title = f"{forecast[0]} in {input}", description = "", color = 0x00ff00)
+        
+        for a in forecast:
+            x = a.split()
+            if x[0] != "Forecast":
+                embed.add_field(name = x[0], value = x[1], inline = False)
+            
+        await ctx.send(embed = embed)
+
 
 def setup(bot):
     bot.add_cog(weather(bot))
