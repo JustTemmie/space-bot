4879321f81701a7d5135fc50f9c41c615cd461e5
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-05-09 17:03:59 +0200

    we

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ async def on_ready():
 
         guild_count = 0
         for guild in bot.guilds:
-            print(f"- {guild.id}) (name: {guild.name})")
+            print(f"- {guild.id} (name: {guild.name})")
             guild_count = guild_count + 1
 
         print(f"{bot.user} is in " + str(guild_count) + " guild(s).")
