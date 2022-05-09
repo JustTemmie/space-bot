cef5d49e70fd6b478f83efafece06c5db58d84a6
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-05-09 19:25:43 +0200

    8ball

---
+++ cogs/8ball.py
@@ -0,0 +1,52 @@
+from urllib import response
+import discord
+from discord import Member, Embed
+from discord.ext import commands
+from discord.ext.commands import cooldown, BucketType
+
+import random
+
+responses = [
+    'It is certain.',
+    'It is decidedly so.',
+    'Without a doubt.',
+    'Yes definitely.',
+    'You may rely on it.',
+    'As I see it, yes.',
+    'Most likely.',
+    'Outlook good.',
+    'Yes.',
+    'Signs point to yes.',
+    'Reply hazy try again.',
+    'Ask again later.',
+    'Better not tell you now.',
+    'Cannot predict now.',
+    'Concentrate and ask again.',
+    'Don\'t count on it.',
+    'My reply is no.',
+    'My sources say no.',
+    'Outlook not so good.',
+    'Very doubtful.',
+    'No way.',
+    'Maybe',
+    'The answer is hiding inside you',
+    'No.',
+    '||No||',
+    '||Yes||',
+    'Hang on',
+    'It\'s over',
+    'It\'s just the beginning',
+    'Good Luck',
+]
+
+class ball8(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="8ball", brief="Ask the 8ball a question")
+    async def ball8_command(self, ctx):
+        await ctx.send(random.choice(responses))
+
+
+def setup(bot):
+    bot.add_cog(ball8(bot))
--- cogs/hwstatus.py
+++ cogs/hwstatus.py
@@ -18,7 +18,7 @@ def get_size(bytes, suffix="B"):
         bytes /= factor
 
 
-class info(commands.Cog):
+class hw(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -125,4 +125,4 @@ class info(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(info(bot))
+    bot.add_cog(hw(bot))
