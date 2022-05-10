60e2232f967b0d0b093d6fcdffee1cfabf1c6288
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-05-10 14:03:31 +0200

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -4,7 +4,7 @@
 import argparse
 import subprocess
 import string
-aeirshkvauialufwblefwakicneiaikvalukvifpvulblckefawlvilvnecki
+aeirshkvauialufwblefwakicneiaikvalukvifpvulblckefawlvilvneck
 from collections import defaultdict
 from PIL import Image, ImageDraw, ImageFont
 
