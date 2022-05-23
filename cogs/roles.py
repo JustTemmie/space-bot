806baa210ea2d6b3384fee812f8472ea9f893fa1
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-05-23 10:13:27 +0200

    Update roles.py

--- cogs/roles.py
+++ cogs/roles.py
@@ -13,26 +13,36 @@ class role_menu(commands.Cog):
     async def on_raw_reaction_add(self, payload):
         if payload.member.bot:
             return
-        if payload.channel_id == (919714824915648562):  # frog
-            with open("./storage/reactions/frog_reactions.json") as react_file:
-                data = json.load(react_file)
-
-        elif payload.channel_id == (849692162907176990):  # space
-            with open("./storage/reactions/space_reactions.json") as react_file:
-                data = json.load(react_file)
-
-        elif payload.channel_id == (956467944991358997):  # crawford castle
-            with open("./storage/reactions/castle_reactions.json") as react_file:
-                data = json.load(react_file)
-
-        else:
+        
+        channel_IDS = [
+            919714824915648562,
+            849692162907176990,
+            956467944991358997
+        ]
+        
+        role_directories = [
+            "frog_reactions",
+            "space_reactions",
+            "castle_reactions"
+        ]
+        
+        if payload.channel_id not in channel_IDS:
             return
+        
+        for channel_ID, dir in zip(channel_IDS, role_directories):
+            if payload.channel_id == (channel_ID):  # frog
+                with open(f"./storage/reactions/{dir}.json") as react_file:
+                    data = json.load(react_file)
 
-        for x in data:
-            if x["emoji"] == payload.emoji.name:
-                role = get(payload.member.guild.roles, id=x["role_id"])
-                await payload.member.add_roles(role)
-                print(f"added {role}")
+        try:
+            for x in data:
+                if x["emoji"] == payload.emoji.name:
+                    role = get(payload.member.guild.roles, id=x["role_id"])
+                    await payload.member.add_roles(role)
+                    print(f"added {role}")
+        
+        except:
+            print("no role found, line 40ish roles.py")
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
