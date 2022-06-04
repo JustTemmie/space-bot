c7e3c1f2af921e42280bbc726447114a7c719b64
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-06-05 00:44:01 +0200

    cleaning code

--- libraries/economyLib.py
+++ libraries/economyLib.py
@@ -61,52 +61,29 @@ async def get_shop_data():
 
 async def open_account(user):
     users = await get_bank_data()
-        
+
+    
     if str(user.id) in users:
-        users[str(user.id)]["version"] = 1.00
-        
-        with open("storage/playerInfo/bank.json", "w") as f:
-                    json.dump(users, f)
-
-        users = await get_bank_data()
-
-    try:
-        (users[str(user.id)]["stats"])
-
-    except:
-        if str(user.id) in users:
+        try:
+            users[str(user.id)]["inventory"]
+        except:
+            #if users[str(user.id)]["version"] < 1.00:
             users[str(user.id)]["inventory"] = {}
             
             users[str(user.id)]["quote"] = "I'm not a bot, I'm a human"
             
-            users[str(user.id)]["scavenge_cooldown"] = time.time()
+            users[str(user.id)]["scavenge_cooldown"] = time.time() - 450
             users[str(user.id)]["inventory"]["logs"] = 0
             
             users[str(user.id)]["daily"] = {}
             users[str(user.id)]["daily"]["day"] = 0
             users[str(user.id)]["daily"]["streak"] = 0
 
-            with open("storage/playerInfo/bank.json", "w") as f:
-                json.dump(users, f)
-
-        users = await get_bank_data()
-    
-    try:
-        users[str(user.id)]["spoke_day"]
-    except:
-        if str(user.id) in users:
+            ###############################################################################
+            
             users[str(user.id)]["spoke_day"] = (datetime.utcnow() - datetime(1970, 1, 1)).days - 1
             users[str(user.id)]["spoken_today"] = 0
             
-            with open("storage/playerInfo/bank.json", "w") as f:
-                json.dump(users, f)
-
-        users = await get_bank_data()
-    
-    try:
-        users[str(user.id)]["lodge"]
-    except:
-        if str(user.id) in users:
             users[str(user.id)]["dam"] = {}
             users[str(user.id)]["dam"]["spent"] = {}
             users[str(user.id)]["dam"]["spent"]["logs"] = 0
@@ -116,16 +93,9 @@ async def open_account(user):
             users[str(user.id)]["lodge"]["spent"] = {}
             users[str(user.id)]["lodge"]["spent"]["logs"] = 0
             users[str(user.id)]["lodge"]["level"] = 0
-            with open("storage/playerInfo/bank.json", "w") as f:
-                    json.dump(users, f)
 
-        users = await get_bank_data()
-    
-                
-    try:
-        users[str(user.id)]["stats"]["points"]
-    except:
-        if str(user.id) in users:
+            ###############################################################################
+            
             users[str(user.id)]["inventory"]["insurance"] = 0
             
             a = random.randint(1, 5)
@@ -141,14 +111,39 @@ async def open_account(user):
             users[str(user.id)]["stats"]["wisdom"] = d
             users[str(user.id)]["stats"]["charisma"] = e
             users[str(user.id)]["stats"]["perception"] = f
-    
+
             users[str(user.id)]["stats"]["points"] = 30 - (a + b + c + d + e + f)
             
+        try:
+            users[str(user.id)]["data"]
+        except: 
+        #if users[str(user.id)]["version"] < 1.01:
+            users[str(user.id)]["data"] = {} 
+            
+            users[str(user.id)]["dailyvote"] = {}
+            users[str(user.id)]["dailyvote"]["streak"] = 0
+            users[str(user.id)]["dailyvote"]["last_vote"] = 0
+            users[str(user.id)]["dailyvote"]["total_votes"] = 0
+
+                    
             with open("storage/playerInfo/bank.json", "w") as f:
-                    json.dump(users, f)
+                json.dump(users, f)
 
             users = await get_bank_data()
+        
+    
+    
+    
+    if str(user.id) in users:
+        users[str(user.id)]["version"] = 1.02
+        
+        with open("storage/playerInfo/bank.json", "w") as f:
+                    json.dump(users, f)
 
+        users = await get_bank_data()
+    
+    
+    
     if str(user.id) in users:
         return
 
@@ -218,6 +213,16 @@ async def open_account(user):
     users[str(user.id)]["stats"]["points"] = 30 - (a + b + c + d + e + f)
     
     ###############################################################################
+    
+    users[str(user.id)]["version"] = 1.01 
+    
+    users[str(user.id)]["data"] = {} 
+    
+    users[str(user.id)]["dailyvote"] = {}
+    users[str(user.id)]["dailyvote"]["streak"] = 0
+    users[str(user.id)]["dailyvote"]["last_vote"] = 0
+    users[str(user.id)]["dailyvote"]["total_votes"] = 0
+
     
 
     with open("storage/playerInfo/bank.json", "w") as f:
