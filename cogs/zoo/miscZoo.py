efd5fe3f14b4208702709522ec39c4568f5e3473
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-07-03 00:11:50 +0200

    split zoo command into zoo and dex, you can also get info about other users

--- cogs/zoo/miscZoo.py
+++ cogs/zoo/miscZoo.py
@@ -1,3 +1,4 @@
+from collections import UserList
 import discord
 from discord.ext import commands
 from discord.ext.commands import cooldown, BucketType
@@ -11,53 +12,76 @@ class miscZoo(commands.Cog):
         self.bot = bot
 
     
-    @commands.command(name="zoo", brief="zoo")
+    @commands.command(
+        name="zoo",
+        brief="check your zoo"
+    )
     @cooldown(2, 10, BucketType.user)
-    async def checkZoo(self, ctx, input = None):
+    async def checkZoo(self, ctx, user: discord.Member = None):
+        if user is None:
+            user = ctx.author
+            
         await aniLib.open_zoo(self, ctx)
         
-        if await aniLib.check_if_zoo_not_exist(ctx.author):
-            return await ctx.send("you need to create an account first")
+        if await aniLib.check_if_zoo_not_exist(user):
+            return await ctx.send("i could not find an inventory for that user, they need to create an account first")
         
         with open("storage/animals.json", "r") as f:
             zoo = json.load(f)
         
         data = await aniLib.get_animal_data()
         
-        user = ctx.author
 
-        if input == None:
-            end = "s"
-            if user.display_name[-1:] == "s":
-                end = ""
-            message_str = f"||\n||                  🌲  **{user.display_name}'{end} zoo:**  🌲\n\n"
-            
-            animalsInTiers = {
-                "common": [],
-                "uncommon": [],
-                "rare": [],
-                "epic": [],
-                "mythical": [],
-            }
+        end = "s"
+        if user.display_name[-1:] == "s":
+            end = ""
+        message_str = f"||\n||                  🌲  **{user.display_name}'{end} zoo:**  🌲\n\n"
+        
+        animalsInTiers = {
+            "common": [],
+            "uncommon": [],
+            "rare": [],
+            "epic": [],
+            "mythical": [],
+        }
+
+        for tier in zoo:
+            for i in zoo[tier]["animals"]:
+                animal = zoo[tier]["animals"][i]
+                icon = animal["icon"]
+                name = animal["name"][0]
+                caught = data[str(user.id)]['animals'][tier][name]["caught"]
+                if caught != 0 or tier == "common":
+                    animalsInTiers[tier].append(f"{icon}`{data[str(user.id)]['animals'][tier][name]['count']}` ")
+
+        for tier in animalsInTiers:
+            if len(animalsInTiers[tier]) != 0:
+                message_str += f"{zoo[tier]['icon']}    {' '.join(animalsInTiers[tier])}\n"
+                        
+        await ctx.send(message_str)
+        return
     
-            for tier in zoo:
-                for i in zoo[tier]["animals"]:
-                    animal = zoo[tier]["animals"][i]
-                    icon = animal["icon"]
-                    name = animal["name"][0]
-                    caught = data[str(user.id)]['animals'][tier][name]["caught"]
-                    if caught != 0 or tier == "common":
-                        animalsInTiers[tier].append(f"{icon}`{data[str(user.id)]['animals'][tier][name]['count']}` ")
-
-            for tier in animalsInTiers:
-                if len(animalsInTiers[tier]) != 0:
-                    message_str += f"{zoo[tier]['icon']}    {' '.join(animalsInTiers[tier])}\n"
-                            
-            await ctx.send(message_str)
-            return
-        
-        
+    @commands.command(
+        name="dex",
+        brief="check a specific animal"
+    )
+    @cooldown(2, 10, BucketType.user)
+    async def dexCommand(self, ctx, input, user: discord.Member = None):
         input = input.lower()
+        await aniLib.open_zoo(self, ctx)
+        
+        if user is None:
+            user = ctx.author
+
+        if await aniLib.check_if_zoo_not_exist(ctx.author):
+            return await ctx.send("i could not find an inventory for that user, they need to create an account first")
+        
+        with open("storage/animals.json", "r") as f:
+            zoo = json.load(f)
+        
+        data = await aniLib.get_animal_data()
+        
+        
         animalName = "none"
         for tier in zoo:
             for i in zoo[tier]["animals"]:
@@ -77,7 +101,7 @@ class miscZoo(commands.Cog):
         
         embed = discord.Embed()
         embed.title = f"{icon} {animalName}"
-        embed.color = ctx.author.colour
+        embed.color = user.colour
         #embed.description = f"{animalTier}"
         #embed.description = "   Can't find what you're looking for? join our [support server](https://discord.gg/8MdVe6NgVy) for help"
         
@@ -90,12 +114,12 @@ class miscZoo(commands.Cog):
             name="||\n||",
             value=f"""
 **Tier:** {zoo[animalTier]["icon"]} {animalTier}{aliasesString}
-**Caught:** {data[str(ctx.author.id)]["animals"][animalTier][animalName]["caught"]}
+**Caught:** {data[str(user.id)]["animals"][animalTier][animalName]["caught"]}
 **Total Caught:** {data["global"]["animals"][animalTier][animalName]["caught"]}
-**Count:** {data[str(ctx.author.id)]["animals"][animalTier][animalName]["count"]}
-**Sold:** {data[str(ctx.author.id)]["animals"][animalTier][animalName]["sold"]}
-**Sacrificed:** {data[str(ctx.author.id)]["animals"][animalTier][animalName]["sacrificed"]}
-**XP:** {data[str(ctx.author.id)]["animals"][animalTier][animalName]["xp"]}
+**Count:** {data[str(user.id)]["animals"][animalTier][animalName]["count"]}
+**Sold:** {data[str(user.id)]["animals"][animalTier][animalName]["sold"]}
+**Sacrificed:** {data[str(user.id)]["animals"][animalTier][animalName]["sacrificed"]}
+**XP:** {data[str(user.id)]["animals"][animalTier][animalName]["xp"]}
             """)
         
         await ctx.send(embed=embed)
