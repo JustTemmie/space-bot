e101a2506eb9e7e95f5ba3006cfaeb9e40aece4b
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-07-03 15:30:29 +0200

    owner give command

---
+++ cogs/economy/owner.py
@@ -0,0 +1,31 @@
+import discord
+from discord import Member, Embed
+from discord.ext import commands
+from discord.ext.commands import cooldown, BucketType
+import json
+
+
+from libraries.economyLib import *
+import libraries.standardLib as SL
+
+
+class ecoowner(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(
+        name = "ownergive"
+    )
+    @commands.is_owner()
+    async def ownerGiveCommand(self, ctx, member: Member, amount: int):        
+        if await check_if_not_exist(member):
+            return await ctx.send(f"{await SL.removeat(member.display_name)} doesn't have an account, they need to create one first")
+
+        await update_bank_data(member, amount)
+
+        auth = ctx.author.display_name
+
+        await ctx.send(await SL.removeat(f"{ctx.author.display_name} gave {amount} <:beaverCoin:968588341291397151> to {member.display_name}"))
+
+def setup(bot):
+    bot.add_cog(ecoowner(bot))
