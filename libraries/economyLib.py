9f7862c18e32a636aa72c046d6a63279abee6967
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-07-03 16:40:28 +0200

    legal mumbo jumbo

--- libraries/economyLib.py
+++ libraries/economyLib.py
@@ -191,7 +191,7 @@ async def open_account(self, ctx):
         color=ctx.author.color
     )
     
-    embed.add_field(name = "If you do, please respond with \"yes\"\nDoing this means you agree to Andromeda's TOS and privacy policy\n\nhttps://github.com/JustTemmie/space-bot/blob/main/service.md\nhttps://github.com/JustTemmie/space-bot/blob/main/privacy-policy.md", value = "||\n||", inline = False)
+    embed.add_field(name = "If you do, please respond with \"yes\"\nDoing this means you agree to Andromeda's TOS and privacy policy\n(it's like 40 lines, you can read it in 2 min)\n\nhttps://github.com/JustTemmie/space-bot/blob/main/service.md\nhttps://github.com/JustTemmie/space-bot/blob/main/privacy-policy.md", value = "||\n||", inline = False)
     
     await ctx.send(embed=embed)
     input = await get_input(self, ctx, 30, "please try again")
