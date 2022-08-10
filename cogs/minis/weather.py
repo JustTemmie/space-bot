4fca1787320cbe0a25e4d459604d1423a3ac4d72
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-08-10 12:27:47 +0200

    no embed :(

--- cogs/minis/weather.py
+++ cogs/minis/weather.py
@@ -86,16 +86,11 @@ class weather(Cog):
             #yr = Image.open("images/processed/yr.jpg")
             #yr = yr.resize((1653*2, 2339*2))
             #yr.save("images/processed/yr.jpg")
-            
             av_button = discord.ui.Button(label = "Open Externally", url = "https://www.yr.no/nb/utskrift/v%C3%A6rvarsel/1-305409/Norge/Troms%20og%20Finnmark/Troms%C3%B8/Troms%C3%B8", emoji = "📩" )
             view = discord.ui.View()
             view.add_item(av_button)
-            
-            embed = discord.Embed()
-            embed.set_image(url="https://www.yr.no/nb/utskrift/v%C3%A6rvarsel/1-305409/Norge/Troms%20og%20Finnmark/Troms%C3%B8/Troms%C3%B8")
-            embed.color = ctx.author.colour
-            
-            await ctx.send(embed=embed, view=view)
+
+            await ctx.send(file=discord.File("temp/yr.jpg"), view=view)
 
 
 async def setup(bot):
