5290c8234f01cbb999af348e6c913b33d050549d
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-08-11 20:48:29 +0200

    math command is back yay

--- cogs/minis/math.py
+++ cogs/minis/math.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from discord.ext.commands import cooldown, BucketType
 
 from math import *
+from interruptingcow import timeout
 
 replacement_table = {
     "x": "*",
@@ -18,14 +19,15 @@ class mathCommands(commands.Cog):
     )
     @cooldown(120, 1800, BucketType.user)
     async def math_command(self, ctx, *, equation):
-        await ctx.send("disabled for now because of security reasons, sorry")
-        # try:
-        #     with timeout(5, exception=RuntimeError):
-        #         for symbol in replacement_table:
-        #             equation = equation.replace(symbol, replacement_table[symbol])
-        #         await ctx.send(f"= {eval(equation)}")
-        # except Exception as e:
-        #     await ctx.send(f"Error: {e}")
+        if set(equation).difference(set("1234567890/*-+()")):
+            return await ctx.send("invalid characters used, please only use numbers and the following: `+-*/^`")
+        try:
+            with timeout(5, exception=RuntimeError):
+                for symbol in replacement_table:
+                    equation = equation.replace(symbol, replacement_table[symbol])
+                await ctx.send(f"= {eval(equation)}")
+        except Exception as e:
+            await ctx.send(f"Error: {e}")
 
 async def setup(bot):
     await bot.add_cog(mathCommands(bot))
