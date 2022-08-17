3af7bb09a68eef793b43cadce4790185a1396610
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-08-17 10:09:36 +0200

    Update eatStick.py

--- cogs/economy/eatStick.py
+++ cogs/economy/eatStick.py
@@ -37,7 +37,7 @@ class stickyummy(commands.Cog):
         
             r = requests.get(
                 "https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s"
-                % (f"anime eat stick", tenor_api_key, 30)
+                % (f"anime eat stick", tenor_api_key, 20)
             )
 
             if r.status_code == 200:
