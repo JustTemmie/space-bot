d551d11fc753895b978702f3b3cda3221a8777e0
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-08-18 21:33:24 +0200

    update accounts on bot startup

--- libraries/economyLib.py
+++ libraries/economyLib.py
@@ -84,9 +84,7 @@ async def get_shop_data():
 async def check_if_not_exist(user):
     users = await get_bank_data()
 
-    if str(user.id) in users:
-        await update_account(user)
-        
+    if str(user.id) in users:        
         if await isUserBanned(user):
             return "banned"
         
@@ -95,105 +93,107 @@ async def check_if_not_exist(user):
     return True
 
 
-async def update_account(user):
+async def update_accounts():
     users = await get_bank_data()
+    
+    for user in users:
 
-    try:
-        users[str(user.id)]["inventory"]
-    except:
-        #if users[str(user.id)]["version"] < 1.00:
-        users[str(user.id)]["inventory"] = {}
-        
-        users[str(user.id)]["quote"] = "I'm not a bot, I'm a human"
-        
-        users[str(user.id)]["scavenge_cooldown"] = time.time() - 900
-        users[str(user.id)]["inventory"]["logs"] = 0
-        
-        users[str(user.id)]["daily"] = {}
-        users[str(user.id)]["daily"]["day"] = 0
-        users[str(user.id)]["daily"]["streak"] = 0
-
-        ###############################################################################
-        
-        users[str(user.id)]["spoke_day"] = (datetime.utcnow() - datetime(1970, 1, 1)).days - 1
-        users[str(user.id)]["spoken_today"] = 0
-        
-        users[str(user.id)]["dam"] = {}
-        users[str(user.id)]["dam"]["spent"] = {}
-        users[str(user.id)]["dam"]["spent"]["logs"] = 0
-        users[str(user.id)]["dam"]["level"] = 0
-        
-        users[str(user.id)]["lodge"] = {}
-        users[str(user.id)]["lodge"]["spent"] = {}
-        users[str(user.id)]["lodge"]["spent"]["logs"] = 0
-        users[str(user.id)]["lodge"]["level"] = 0
-
-        ###############################################################################
-        
-        users[str(user.id)]["inventory"]["insurance"] = 0
-        
-        users[str(user.id)]["stats"] = {}
-        
-        a = random.randint(1, 5)
-        b = random.randint(1, 5)
-        c = random.randint(1, 5)
-        d = random.randint(1, 5)
-        e = random.randint(1, 5)
-        f = random.randint(1, 5)
-        
-        users[str(user.id)]["stats"]["strength"] = a
-        users[str(user.id)]["stats"]["dexterity"] = b
-        users[str(user.id)]["stats"]["intelligence"] = c
-        users[str(user.id)]["stats"]["wisdom"] = d
-        users[str(user.id)]["stats"]["charisma"] = e
-        users[str(user.id)]["stats"]["perception"] = f
-
-        users[str(user.id)]["stats"]["points"] = 30 - (a + b + c + d + e + f)
+        try:
+            users[str(user)]["inventory"]
+        except:
+            #if users[str(user)]["version"] < 1.00:
+            users[str(user)]["inventory"] = {}
             
-    try:
-        users[str(user.id)]["data"]
-    except: 
-    #if users[str(user.id)]["version"] < 1.01:
-        users[str(user.id)]["data"] = {} 
-        
-        users[str(user.id)]["dailyvote"] = {}
-        users[str(user.id)]["dailyvote"]["streak"] = 0
-        users[str(user.id)]["dailyvote"]["last_vote"] = 0
-        users[str(user.id)]["dailyvote"]["total_votes"] = 0
-        
-                
-        with open("storage/playerInfo/bank.json", "w") as f:
-            json.dump(users, f)
+            users[str(user)]["quote"] = "I'm not a bot, I'm a human"
+            
+            users[str(user)]["scavenge_cooldown"] = time.time() - 900
+            users[str(user)]["inventory"]["logs"] = 0
+            
+            users[str(user)]["daily"] = {}
+            users[str(user)]["daily"]["day"] = 0
+            users[str(user)]["daily"]["streak"] = 0
 
-        users = await get_bank_data()
+            ###############################################################################
+            
+            users[str(user)]["spoke_day"] = (datetime.utcnow() - datetime(1970, 1, 1)).days - 1
+            users[str(user)]["spoken_today"] = 0
+            
+            users[str(user)]["dam"] = {}
+            users[str(user)]["dam"]["spent"] = {}
+            users[str(user)]["dam"]["spent"]["logs"] = 0
+            users[str(user)]["dam"]["level"] = 0
+            
+            users[str(user)]["lodge"] = {}
+            users[str(user)]["lodge"]["spent"] = {}
+            users[str(user)]["lodge"]["spent"]["logs"] = 0
+            users[str(user)]["lodge"]["level"] = 0
+
+            ###############################################################################
+            
+            users[str(user)]["inventory"]["insurance"] = 0
+            
+            users[str(user)]["stats"] = {}
+            
+            a = random.randint(1, 5)
+            b = random.randint(1, 5)
+            c = random.randint(1, 5)
+            d = random.randint(1, 5)
+            e = random.randint(1, 5)
+            f = random.randint(1, 5)
+            
+            users[str(user)]["stats"]["strength"] = a
+            users[str(user)]["stats"]["dexterity"] = b
+            users[str(user)]["stats"]["intelligence"] = c
+            users[str(user)]["stats"]["wisdom"] = d
+            users[str(user)]["stats"]["charisma"] = e
+            users[str(user)]["stats"]["perception"] = f
+
+            users[str(user)]["stats"]["points"] = 30 - (a + b + c + d + e + f)
+                
+        try:
+            users[str(user)]["data"]
+        except: 
+        #if users[str(user)]["version"] < 1.01:
+            users[str(user)]["data"] = {} 
+            
+            users[str(user)]["dailyvote"] = {}
+            users[str(user)]["dailyvote"]["streak"] = 0
+            users[str(user)]["dailyvote"]["last_vote"] = 0
+            users[str(user)]["dailyvote"]["total_votes"] = 0
+            
+                    
+            with open("storage/playerInfo/bank.json", "w") as f:
+                json.dump(users, f)
+
+            users = await get_bank_data()
+            
+        if users[str(user)]["version"] <= 1.02:
+            users[str(user)]["anti-cheat"] = {}
+            users[str(user)]["anti-cheat"]["counter"] = 0
+            users[str(user)]["anti-cheat"]["last_command"] = time.time()
         
-    if users[str(user.id)]["version"] <= 1.02:
-        users[str(user.id)]["anti-cheat"] = {}
-        users[str(user.id)]["anti-cheat"]["counter"] = 0
-        users[str(user.id)]["anti-cheat"]["last_command"] = time.time()
-    
-    if users[str(user.id)]["version"] <= 1.05:
-        users[str(user.id)]["anti-cheat"]["banned_until"] = 0
-        users[str(user.id)]["anti-cheat"]["banned_x_times"] = 0
-    
-    if users[str(user.id)]["version"] <= 1.06:
-        users[str(user.id)]["statistics"] = {}
-        users[str(user.id)]["statistics"]["total_logs"] = 0
-    
-    if users[str(user.id)]["version"] <= 1.07:
-        users[str(user.id)]["statistics"]["total_coins"] = 0
-    
-    if users[str(user.id)]["version"] <= 1.08:
-        users[str(user.id)]["inventory"]["stick"] = 0
-    
-    if users[str(user.id)]["version"] <= 1.09:
-        users[str(user.id)]["statistics"]["total_sticks_eaten"] = 0
-    
-    if str(user.id) in users:
-        users[str(user.id)]["version"] = inv_version
+        if users[str(user)]["version"] <= 1.05:
+            users[str(user)]["anti-cheat"]["banned_until"] = 0
+            users[str(user)]["anti-cheat"]["banned_x_times"] = 0
         
-        with open("storage/playerInfo/bank.json", "w") as f:
-                    json.dump(users, f)
+        if users[str(user)]["version"] <= 1.06:
+            users[str(user)]["statistics"] = {}
+            users[str(user)]["statistics"]["total_logs"] = 0
+        
+        if users[str(user)]["version"] <= 1.07:
+            users[str(user)]["statistics"]["total_coins"] = 0
+        
+        if users[str(user)]["version"] <= 1.08:
+            users[str(user)]["inventory"]["stick"] = 0
+        
+        if users[str(user)]["version"] <= 1.09:
+            users[str(user)]["statistics"]["total_sticks_eaten"] = 0
+        
+        if str(user) in users:
+            users[str(user)]["version"] = inv_version
+            
+            with open("storage/playerInfo/bank.json", "w") as f:
+                        json.dump(users, f)
 
 
 
