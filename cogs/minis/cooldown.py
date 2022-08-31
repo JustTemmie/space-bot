d2931ce8e5a36660eda329fc82597b4e8a582c73
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-09-01 01:04:23 +0200

    Update cooldown.py

--- cogs/minis/cooldown.py
+++ cogs/minis/cooldown.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from discord.ext.commands import cooldown, BucketType
 
 from time import time
+from datetime import datetime
 
 import libraries.economyLib as EL
 import libraries.standardLib as SL 
@@ -43,21 +44,27 @@ class showCooldowns(commands.Cog):
         
         command = self.bot.get_command("scavenge")
         command_cooldown = await self.get_cooldown(command, ctx)
-        desc = f"{await self.time_conversion(command_cooldown)}"
         if command_cooldown > 0: desc = f" <t:{command_cooldown+round(time())}:R>"
+        else: desc = "✅"
         embed.add_field(name="Scavenge", value=desc, inline=False)
         
         command = self.bot.get_command("hunt")
         command_cooldown = await self.get_cooldown(command, ctx)
-        desc = f"{await self.time_conversion(command_cooldown)}"
         if command_cooldown > 0: desc = f" <t:{command_cooldown+round(time())}:R>"
+        else: desc = "✅"
         embed.add_field(name="Hunt", value=desc, inline=False)
         
         command = self.bot.get_command("eat")
         command_cooldown = await self.get_cooldown(command, ctx)
-        desc = f"{await self.time_conversion(command_cooldown)}"
-        if command_cooldown > 0: desc = f" <t:{command_cooldown+round(time())}:R>"
+        if command_cooldown > 0: desc = f"<t:{command_cooldown+round(time())}:R>"
+        else: desc = "✅"
         embed.add_field(name="Eat", value=desc, inline=False)
+        
+        daily_info = data[str(ctx.author.id)]["daily"]
+        if daily_info["day"] == (datetime.utcnow() - datetime(1970, 1, 1)).days:
+            desc = f"<t:{round(await self.time_until_end_of_day() + time())}:R>"
+        else: desc = "✅"
+        embed.add_field(name="Daily", value=desc, inline=False)
         
 
         await ctx.send(embed=embed)
@@ -65,6 +72,12 @@ class showCooldowns(commands.Cog):
     async def get_cooldown(self, command, ctx):
         return round(command.get_cooldown_retry_after(ctx))
     
+    # https://stackoverflow.com/questions/45986035/seconds-until-end-of-day-in-python
+    async def time_until_end_of_day(self):
+        dt = datetime.utcnow()
+        return ((24 - dt.hour - 1) * 60 * 60) + ((60 - dt.minute - 1) * 60) + (60 - dt.second)
+
+    # unused lmao
     async def time_conversion(self, sec):
         sec_value = sec % (24 * 3600)
         hour_value = sec_value // 3600
