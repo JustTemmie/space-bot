1d66a0b3457b9ba3721423435074c11f0a396fc2
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-11-11 10:19:16 +0100

    tweaked some numbers in the pdf conversion

--- cogs/minis/pdfConversion.py
+++ cogs/minis/pdfConversion.py
@@ -22,10 +22,10 @@ class pdfConversion(commands.Cog):
         brief="converts your pdf into a png",
     )
     @cooldown(1, 5, BucketType.user)
-    async def calendar(self, ctx, dpi=200):
-        if dpi >= 400 and 725539745572323409 != ctx.author.id:
-            await ctx.send("lowering dpi to 400")
-            dpi = 400
+    async def calendar(self, ctx, dpi=450):
+        if dpi >= 450 and 725539745572323409 != ctx.author.id:
+            await ctx.send("lowering dpi to 450")
+            dpi = 450
         if ctx.message.attachments:
             attachment = ctx.message.attachments[0]
             data = await attachment.read()
@@ -34,11 +34,11 @@ class pdfConversion(commands.Cog):
             return await ctx.send("You forgot the image.")
 
         async with ctx.typing():
-            with open(f"temp/{ctx.author.id}.pdf", "wb") as f:
+            with open(f"temp/{ctx.author.id}-{filename}.pdf", "wb") as f:
                 f.write(data)
-            convert_from_path(f"temp/{ctx.author.id}.pdf", dpi=dpi)[0].save(f"temp/{ctx.author.id}.png", "PNG")
+            convert_from_path(f"temp/{ctx.author.id}-{filename}.pdf", dpi=dpi)[0].save(f"temp/{ctx.author.id}-{filename}.png", "PNG")
 
-            await ctx.reply(file=discord.File(f"temp/{ctx.author.id}.png"))
+            await ctx.reply(file=discord.File(f"temp/{ctx.author.id}-{filename}.png"))
             
     
 
