a158075621b79d00016edaacc23f06bbeab2f7e6
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2024-01-18 17:23:01 +0100

    fix shop typos

--- cogs/economy/shop.py
+++ cogs/economy/shop.py
@@ -247,7 +247,7 @@ class ecoshop(commands.Cog):
                 differentAnimalsSold += animalAmount[1]
 
         if floor(payout * (1 + merchant * 0.01)) - payout > random.randint(15, 25):
-            extraProfit = floor(payout*(1+merchant*0.01))-payout
+            extraProfit = floor(payout*(merchant*0.01))
             payout = floor(payout + extraProfit)
             # if earned more than 100 extra coins
             if extraProfit > 100:
@@ -256,7 +256,7 @@ class ecoshop(commands.Cog):
                 soldstr += f"\n\"Since i'm feeling generous, i gave you an extra {extraProfit} <:beaverCoin:1019212566095986768>\"\n"
             
         if bank[str(ctx.author.id)]["lodge"]["level"] >= 6:
-            soldstr += f"\nSince you had a lodge level 7 or above, i gave you an extra 15% payout! ({floor(payout*0.15)} <:beaverCoin:1019212566095986768>)\n"
+            soldstr += f"\nSince you had a lodge level 6 or above, i gave you an extra 15% payout! ({floor(payout*0.15)} <:beaverCoin:1019212566095986768>)\n"
             payout = floor(payout * 1.15)
 
         with open("storage/playerInfo/animals.json", "w") as f:
